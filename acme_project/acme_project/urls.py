7b005594955688246f06f87c56dc2ee420c3f927
AlxLen <lenko29@gmail.com>
2023-10-18 16:18:32 +0300

    end of 7th sprint

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,12 +3,38 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 
+# Добавьте новые строчки с импортами классов.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
 from django.contrib import admin
-from django.urls import include, path
+# К импортам из django.urls добавьте импорт функции reverse_lazy
+from django.urls import include, path, reverse_lazy
+
+handler404 = 'core.views.page_not_found'
+
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-    # В конце добавляем к списку вызов функции static.
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
+]
+# Подключаем дебаг-панель:
+if settings.DEBUG:
+    import debug_toolbar
+    # Добавить к списку urlpatterns список адресов
+    # из приложения debug_toolbar:
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+# В конце добавляем к списку вызов функции static.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,7 +1,25 @@
-from django.db import models
+from django.contrib.auth import get_user_model
 
+from django.db import models
+# Импортируем функцию reverse() для получения ссылки на объект.
+from django.urls import reverse
 
 from .validators import real_age
+
+
+# Да, именно так всегда и ссылаемся на модель пользователя!
+User = get_user_model()
+
+
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.tag
 
 
 class Birthday(models.Model):
@@ -11,6 +29,16 @@ class Birthday(models.Model):
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
     image = models.ImageField('Фото', blank=True)
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
+    )
+
 
     class Meta:
         constraints = (
@@ -19,3 +47,21 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
+
+
+class Congratulation(models.Model):
+    text = models.TextField('Текст поздравления')
+    birthday = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
